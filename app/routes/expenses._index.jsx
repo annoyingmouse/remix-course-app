c63d6b4090d6f365d4a311de38a2bc029a896470
Dominic Myers <annoyingmouse@gmail.com>
2025-05-25 08:50:45 +0100

    ✨ (expenses._index.jsx): add Outlet component from @remix-run/react to ExpensesPage component for nested routing support 🔧 (remix.config.js): add new remix.config.js file to the project for configuring Remix framework settings

--- app/routes/expenses._index.jsx
+++ app/routes/expenses._index.jsx
@@ -1,3 +1,8 @@
+import {Outlet} from "@remix-run/react";
+
 export default function ExpensesPage() {
-  return <h1>Expenses Page</h1>;
+  return <main>
+    <p>Shared element!.</p>
+    <Outlet />
+  </main>;
 }
